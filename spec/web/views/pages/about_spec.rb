d0f9f5477a1766e0f7a3e14a5e3c1799fbd4f2b1
Matthew Gibbons <mhgibbons@me.com>
2016-02-02 06:37:49 +0000

    The controller exposes nothing

--- spec/web/views/pages/about_spec.rb
+++ spec/web/views/pages/about_spec.rb
@@ -2,12 +2,8 @@ require 'spec_helper'
 require_relative '../../../../apps/web/views/pages/about'
 
 describe Web::Views::Pages::About do
-  let(:exposures) { Hash[foo: 'bar'] }
+  let(:exposures) { Hash[] }
   let(:template)  { Hanami::View::Template.new('apps/web/templates/pages/about.html.erb') }
   let(:view)      { Web::Views::Pages::About.new(template, exposures) }
   let(:rendered)  { view.render }
-
-  it 'exposes #foo' do
-    view.foo.must_equal exposures.fetch(:foo)
-  end
 end
