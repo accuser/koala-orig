8b82ab7666ab6adad177cbbfc0d84665243474d3
Matthew Gibbons <mhgibbons@me.com>
2016-02-10 15:25:26 +0000

    Authenticate the user

--- spec/web/controllers/session/create_spec.rb
+++ spec/web/controllers/session/create_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
 
 describe Web::Controllers::Session::Create do
-  let(:sign_in) { double 'service' }
-  let(:action) { described_class.new(sign_in: sign_in) }
+  let(:authenticate_user) { double 'service' }
+  let(:action) { described_class.new(authenticate_user: authenticate_user) }
 
   context "with invalid parameters" do
     let(:params) { Hash[ user: {} ] }
@@ -15,19 +15,19 @@ describe Web::Controllers::Session::Create do
 
   context "with valid parameters" do
     let(:params) { Hash[ user: { email: 'alice@example.com', password: 'password' } ] }
-  
+
     context "that match an existing user entity" do
-      let(:user) { double 'user', id: 42, token: 'T0K3N' }
-  
+      let(:user) { double 'user', id: 42, auth_token: 'T0K3N' }
+
       it "authenticates the current user" do
-        expect(sign_in).to receive(:call).and_return(user)
+        expect(authenticate_user).to receive(:call).and_return(user)
 
         response = action.call(params)
         expect(response[0]).to eq 302
       end
 
       it "redirects the client to the new user path" do
-        expect(sign_in).to receive(:call).and_return(user)
+        expect(authenticate_user).to receive(:call).and_return(user)
 
         response = action.call(params)
         expect(response[1]['Location']).to eq '/users/42'
@@ -36,7 +36,7 @@ describe Web::Controllers::Session::Create do
 
     context "that don't match an existing user entity" do
       it "fails with HTTP 422" do
-        expect(sign_in).to receive(:call).and_return(nil)
+        expect(authenticate_user).to receive(:call).and_return(nil)
 
         response = action.call(params)
         expect(response[0]).to eq 422
